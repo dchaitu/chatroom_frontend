7119dc202d28a984e2c9c9d005302780f9946575
dchaitu <chaitanyadokara@gmail.com>
2025-08-25 10:30:49 +0530

    project before downgrading tailwind css

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,6 +1,6 @@
 export const siteKey = '6Lcp4Y0rAAAAAD_kq7y5aB5QlfGM5QLlbxywSlBu'
 export const secretKey = '6Lcp4Y0rAAAAAMx574CaTgPELQT7aT24Aprreo84'
-export const LOCAL_API_PATH = 'http://0.0.0.0:8080'
+export const LOCAL_API_PATH = 'http://localhost:8080'
 
 export const REST_API_PATH = LOCAL_API_PATH
     // = 'https://d1119200q7.execute-api.us-east-1.amazonaws.com/dev'
